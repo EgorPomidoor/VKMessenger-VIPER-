22e26785e3dc00b83af70594abd48f3d58608929
user <user@macbook-air-user.local>
2018-01-14 14:45:07 +0300

    Initial Commit

---
+++ VKMessenger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VKMessenger
+//
+//  Created by Егор on 14.01.2018.
+//  Copyright © 2018 Егор. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
